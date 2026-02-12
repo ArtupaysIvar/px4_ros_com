92501e31732bd9d0b02f561f7f6c92fd821c14b0
ArtupaysIvar <maulanaravi.2006@gmail.com>
2026-02-12 16:44:27 +0700

    refactoring tapi kok jadi ga jalan yaaa

--- src/examples/offboard/offboard_control1.cpp
+++ src/examples/offboard/offboard_control1.cpp
@@ -21,6 +21,15 @@ private:
     // void trajectory_setpoint();
     void relative_setpoint();
 
+    enum class OffboardState {
+    INIT,
+    OFFBOARD,
+    ARMED
+};
+
+    OffboardState state_{OffboardState::INIT};
+    int setpoint_counter_{0};
+
     // publishers
     rclcpp::Publisher<px4_msgs::msg::OffboardControlMode>::SharedPtr offboard_control_mode_pub_;
     rclcpp::Publisher<px4_msgs::msg::TrajectorySetpoint>::SharedPtr trajectory_setpoint_pub_;
@@ -33,24 +42,34 @@ private:
     // buat dapet orientasi dan kalkulasi rotational angle
     float current_yaw_{0.0};
     float current_z{0.0};
+    float target_z;
     Eigen::Matrix2f yaw_rotational_matrix;
     Eigen::Vector3f global_position;
     Eigen::Vector2f global_position_2d;
-
-    Eigen::Vector2f global_pos_2d_;
-    Eigen::Vector2f body_setpoint;   
     Eigen::Matrix2f yaw_rot_matrix;
     Eigen::Vector2f target_pos;
+    Eigen::Vector2f body_pos_setpoint;
+    // Eigen::Vector3f body_vel_setpoint;
 };
 
 Drone1Control::Drone1Control(): Node("drone1_control_node") 
 {
+
+    /*
     offboard_control_mode_pub_ = create_publisher<px4_msgs::msg::OffboardControlMode>(
         "/px4_1/fmu/in/offboard_control_mode", 10);
     trajectory_setpoint_pub_ = create_publisher<px4_msgs::msg::TrajectorySetpoint>(
         "/px4_1/fmu/in/trajectory_setpoint", 10);
     vehicle_command_pub_ = create_publisher<px4_msgs::msg::VehicleCommand>(
         "/px4_1/fmu/in/vehicle_command", 10);
+    */
+
+    offboard_control_mode_pub_ = create_publisher<px4_msgs::msg::OffboardControlMode>(
+        "/fmu/in/offboard_control_mode", 10);
+    trajectory_setpoint_pub_ = create_publisher<px4_msgs::msg::TrajectorySetpoint>(
+        "/fmu/in/trajectory_setpoint", 10);
+    vehicle_command_pub_ = create_publisher<px4_msgs::msg::VehicleCommand>(
+        "/fmu/in/vehicle_command", 10);
 
     vehicle_odom_sub_ = this->create_subscription<px4_msgs::msg::VehicleOdometry>(
         "/fmu/out/vehicle_odometry", rclcpp::QoS(10).best_effort(),
@@ -59,8 +78,52 @@ Drone1Control::Drone1Control(): Node("drone1_control_node")
     timer_ = create_wall_timer(100ms, std::bind(&Drone1Control::relative_setpoint, this));
     
     // JANGAN LUPA SET WAYPOINT NYA
-    body_setpoint << 0, 1; 
+    body_pos_setpoint << 1.0f, 1.0f;
+    target_z = -1.0f;
+    // body_vel_setpoint << 1.0f, 1.0f, 1.0f; 
 }
+
+void Drone1Control::arm() {
+    // PUBLISHER_COUNT (vehicle command: arm)
+    px4_msgs::msg::VehicleCommand arm_msg{};
+    arm_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    arm_msg.param1 = 1.0;  // arm
+    arm_msg.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM;
+    arm_msg.target_system = 1;     
+    arm_msg.target_component = 1;
+    arm_msg.source_system = 1;
+    arm_msg.source_component = 1;
+    vehicle_command_pub_->publish(arm_msg);
+    RCLCPP_INFO(this->get_logger(), "Arm command sent");
+}
+
+void Drone1Control::set_offboard_mode() {
+    // PUBLISHER_COUNT (vehicle command :vehicle mode)
+    px4_msgs::msg::VehicleCommand vehicle_mode_msg{};
+    vehicle_mode_msg.timestamp = this->get_clock()->now().nanoseconds() / 1000;
+    vehicle_mode_msg.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE;
+    vehicle_mode_msg.param1 = 1.0;  // custom
+    vehicle_mode_msg.param2 = 6.0;  // offboard
+    vehicle_mode_msg.target_system = 1;
+    vehicle_mode_msg.target_component = 1;
+    vehicle_mode_msg.source_system = 1;
+    vehicle_mode_msg.source_component = 1;
+    vehicle_command_pub_->publish(vehicle_mode_msg);
+    RCLCPP_INFO(this->get_logger(), "Offboard mode command sent");
+}
+
+void Drone1Control::offboard_control() {
+    // PUBLISHER_COUNT (offboard control)
+    px4_msgs::msg::OffboardControlMode offboard_msg{};
+    offboard_msg.position = true;
+    // set true apa ya?
+    // offboard_msg.velocity = true;
+    offboard_msg.acceleration = false;
+    offboard_msg.attitude = false;
+    offboard_msg.body_rate = false;
+    offboard_control_mode_pub_->publish(offboard_msg);
+}
+
 void Drone1Control::odom_callback(const px4_msgs::msg::VehicleOdometry::SharedPtr odom_msg)
 {
     float qw = odom_msg->q[0];
@@ -82,50 +145,46 @@ void Drone1Control::odom_callback(const px4_msgs::msg::VehicleOdometry::SharedPt
 }
 
 void Drone1Control::relative_setpoint(){
-    // body_setpoint << 0, 1; 
-    target_pos = global_position_2d + (yaw_rotational_matrix * body_setpoint);
+	// nambah ini
+	offboard_control();
+    
+    if (setpoint_counter_ < 10) {
+        px4_msgs::msg::TrajectorySetpoint traj{};
+        traj.timestamp = this->get_clock()->now().nanoseconds() / 1000; 
+        traj.position = {
+            global_position_2d[0],
+            global_position_2d[1],
+            current_z
+        };
+        trajectory_setpoint_pub_->publish(traj);
+
+        setpoint_counter_++;
+        return;
+    }
+    
+    if (state_ == OffboardState::INIT) {
+        RCLCPP_INFO(this->get_logger(), "Setting offboard mode...");
+        set_offboard_mode();
+        state_ = OffboardState::OFFBOARD;
+        return;
+    }
+
+    if (state_ == OffboardState::OFFBOARD) {
+        RCLCPP_INFO(this->get_logger(), "Arming...");
+        arm();
+        state_ = OffboardState::ARMED;
+        return;
+    }
+
+    target_pos = global_position_2d + (yaw_rotational_matrix * body_pos_setpoint);
+    // PUBLISHER_COUNT (traj. setpoint)
     px4_msgs::msg::TrajectorySetpoint traj{};
+    traj.timestamp = this->get_clock()->now().nanoseconds() / 1000;
     traj.position = {target_pos[0], target_pos[1], current_z};
+    // traj.velocity = {body_vel_setpoint[0], body_vel_setpoint[1], body_vel_setpoint[2]};
     trajectory_setpoint_pub_->publish(traj);
 }
 
-
-
-void Drone1Control::offboard_control() {
-// publisher pertama (offboard)
-    px4_msgs::msg::OffboardControlMode offboard_msg{};
-    offboard_msg.position = true;
-    offboard_msg.velocity = false;
-    offboard_msg.acceleration = false;
-    offboard_msg.attitude = false;
-    offboard_msg.body_rate = false;
-    offboard_control_mode_pub_->publish(offboard_msg);
-}
-
-void Drone1Control::arm() {
-    px4_msgs::msg::VehicleCommand arm_msg{};
-    arm_msg.param1 = 1.0;  // arm
-    arm_msg.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_COMPONENT_ARM_DISARM;
-    arm_msg.target_system = 2;     
-    arm_msg.target_component = 1;
-    arm_msg.source_system = 2;
-    arm_msg.source_component = 1;
-    vehicle_command_pub_->publish(arm_msg);
-    // RCLCPP_INFO(this->get_logger(), "Arm command sent");
-}
-
-void Drone1Control::set_offboard_mode() {
-    px4_msgs::msg::VehicleCommand vehicle_mode_msg{};
-    vehicle_mode_msg.command = px4_msgs::msg::VehicleCommand::VEHICLE_CMD_DO_SET_MODE;
-    vehicle_mode_msg.param1 = 1.0;  // custom
-    vehicle_mode_msg.param2 = 6.0;  // offboard
-    vehicle_mode_msg.target_system = 2;
-    vehicle_mode_msg.target_component = 1;
-    vehicle_mode_msg.source_system = 2;
-    vehicle_mode_msg.source_component = 1;
-    vehicle_command_pub_->publish(vehicle_mode_msg);
-    // RCLCPP_INFO(this->get_logger(), "Offboard mode command sent");
-}
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
