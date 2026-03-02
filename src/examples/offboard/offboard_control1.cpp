f28c723303fb7eb29cc40a28b7b036a2cc37e728
ArtupaysIvar <maulanaravi.2006@gmail.com>
2026-03-02 17:26:48 +0700

    pc change

--- src/examples/offboard/offboard_control1.cpp
+++ src/examples/offboard/offboard_control1.cpp
@@ -143,13 +143,13 @@ Drone1Control::Drone1Control(): Node("drone1_control_node")
     // JANGAN LUPA SET WAYPOINT NYA
     body_3dpos_setpoint.reserve(20);
     // kalo z = 0 berarti dia simply tidak climb (bukan berarti landing) 
-    body_3dpos_setpoint.emplace_back(0.0f, 0.0f, -5.0f);
-    body_3dpos_setpoint.emplace_back(2.0f, 0.0f, 0.0f);
-    body_3dpos_setpoint.emplace_back(-3.0f, 0.0f, 0.0f);
-    body_3dpos_setpoint.emplace_back(1.0f, 0.0f, 0.0f);
-    body_3dpos_setpoint.emplace_back(5.0f, 0.0f, 0.0f);
-    body_3dpos_setpoint.emplace_back(0.0f, 3.0f, 0.0f);
-    body_3dpos_setpoint.emplace_back(0.0f, 0.0f, 5.0f);
+    body_3dpos_setpoint.emplace_back(0.0f, 0.0f, -3.0f);
+    // body_3dpos_setpoint.emplace_back(2.0f, 0.0f, 0.0f);
+    // body_3dpos_setpoint.emplace_back(-3.0f, 0.0f, 0.0f);
+    // body_3dpos_setpoint.emplace_back(1.0f, 0.0f, 0.0f);
+    // body_3dpos_setpoint.emplace_back(5.0f, 0.0f, 0.0f);
+    // body_3dpos_setpoint.emplace_back(0.0f, 3.0f, 0.0f);
+    // body_3dpos_setpoint.emplace_back(0.0f, 0.0f, 5.0f);
 }
 
 void Drone1Control::arm() {
